6d9b8e5043c6ef149519f8440d847f0f11316730
Tianle <tianleyin8888@gmail.com>
2024-02-05 20:41:46 +0100

    First commit

---
+++ www/js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+    $('.sidenav').sidenav();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+ 
+ 
+document.addEventListener('deviceready', onDeviceReady, false);
+ 
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+ 
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    //document.getElementById('deviceready').classList.add('ready');
+}
