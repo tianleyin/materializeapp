55f5205e97f9bd7c95bd613a4df49a21c651dd0f
Tianle <tianleyin8888@gmail.com>
2024-02-07 19:28:28 +0100

    Hola

--- www/js/init.js
+++ www/js/init.js
@@ -1,15 +1,23 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
     $('.sidenav').sidenav();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
- 
- 
+
+
 document.addEventListener('deviceready', onDeviceReady, false);
- 
+
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
- 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    //document.getElementById('deviceready').classList.add('ready');
+  // Cordova is now initialized. Have fun!
+
+  console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+  //document.getElementById('deviceready').classList.add('ready');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('.sidenav').sidenav();
+
+  var options = { "swipeable": true };
+  var el = document.querySelectorAll('tabs');
+  var instance = M.Tabs.init(el, options);
+})
